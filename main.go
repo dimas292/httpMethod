a05d791e6f953d8eabbcb297729b6a958be3a5a8
deemisterious <dimasixc22@gmail.com>
2024-08-02 18:45:25 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func main(){
+	
+	http.HandleFunc("/", routeIndexGet)
+	http.HandleFunc("/process", routeSubmitPost)
+
+	fmt.Sprintln("server berjalan pada port 9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func routeIndexGet(w http.ResponseWriter, r *http.Request){
+	if r.Method == "GET" {
+		var templ = template.Must(template.New("form").ParseFiles("view.html"))
+		var err = templ.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return 
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        var tmpl = template.Must(template.New("result").ParseFiles("view.html"))
+
+        if err := r.ParseForm(); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        var name = r.FormValue("name")
+        var message = r.Form.Get("message")
+
+        var data = map[string]string{"name": name, "message": message}
+
+        if err := tmpl.Execute(w, data); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return
+    }
+
+    http.Error(w, "", http.StatusBadRequest)
+}
